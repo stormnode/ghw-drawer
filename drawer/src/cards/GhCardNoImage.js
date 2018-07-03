111a157bd1f35b23b399d89679296d2615d8546b
Michael Angerman <stormasm@gmail.com>
2018-07-02 19:07:05 -0700

    its all now back to working

--- drawer/src/cards/GhCardNoImage.js
+++ drawer/src/cards/GhCardNoImage.js
@@ -2,7 +2,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Card, { CardContent } from "@material-ui/core/Card";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 const styles = {
